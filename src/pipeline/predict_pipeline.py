85abbb55a3a2a205087747005d0b238ae9aecde0
Bismabashir <bismabashir7268@gmail.com>
2024-02-22 20:03:30 -0800

    final commit

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,20 +8,19 @@ from src.utils import load_object
 class PredictPipeline:
     def predict(self, features):
         try:
-            model_path = os.path.abspath(os.path.join("artifacts", "model.pkl"))
-            preprocessor_path = os.path.abspath(os.path.join("artifacts", "preprocessor.pkl"))
+            model_path ='artifacts\model.pkl'
+            preprocessor_path ='artifacts\proprocessor.pkl'
             print("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             print("After Loading")
+            
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
+            
             return preds
         except Exception as e:
             raise CustomException(e, sys)
-
-
-
 
 class CustomData:
     def __init__(  self,
